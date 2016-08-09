6de2dd2441ce0cd21a5914204725708652bb97ae
Patrick <patrick@unseenvision.com>
2016-08-09 01:46:07 -0700

    add animate on hover functionality

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $gifsView = $('#gifsView');
 gif = 'super+nintendo';
 
 // Initial array of gifs
-var gifs = ['Super Mario World', 'TMNT IV Turtles in Time', 'A Link to the Past', 'Kirby Super Star', 'Super Punch Out',];
+let gifs = ['Super Mario World', 'TMNT IV Turtles in Time', 'A Link to the Past', 'Kirby Super Star', 'Super Punch Out', ];
 
 // FUNCTIONS ============================================================
 
@@ -13,9 +13,9 @@ var gifs = ['Super Mario World', 'TMNT IV Turtles in Time', 'A Link to the Past'
 function renderButtons() {
     $('#buttonsView').empty();
     // Loops through the array of gifs
-    for (var i = 0; i < gifs.length; i++) {
+    for (let i = 0; i < gifs.length; i++) {
         // Then dynamicaly generate buttons for each gif in the array
-        var a = $('<button>');
+        let a = $('<button>');
         a.addClass('btn btn-sm btn-default btn-gif');
         a.attr('data-name', gifs[i]);
         a.text(gifs[i]);
@@ -26,20 +26,24 @@ function renderButtons() {
 // Re-renders the HTML to display the appropriate content. ---------
 function displayGifs() {
     $('#gifsView').empty();
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=30";
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=30";
     $.ajax({
         url: queryURL,
         method: 'GET'
     }).done(function(response) {
-        var results = response.data;
+        let results = response.data;
         gifsView = $('#gifsView');
+        console.log(response);
 
-        for (var i = 0; i < results.length; i++) {
-            var gifDiv = $('<div class="gif hoverable">')
-                var slug = results[i].slug;
-                var span = $('<span class="slug">').text("slug: " + slug);
-            var gifImage = $('<img>');
-            gifImage.attr('src', results[i].images.fixed_height.url);
+        for (let i = 0; i < results.length; i++) {
+            let animated = results[i].images.fixed_height.url;
+            let still = results[i].images.fixed_height_still.url;
+            let gifDiv = $('<div class="hoverable">')
+            let slug = results[i].slug;
+            let span = $('<span class="slug">').text(`slug: ${slug}`);
+            let gifImage = $(`<img class="gif" data-still="${still}" data-animate="${animated}">`);
+
+            gifImage.attr('src', still);
 
             gifDiv.append(span)
             gifDiv.append(gifImage)
@@ -55,6 +59,16 @@ function displayGifs() {
 $(document).on('click', '.btn-gif', function() {
     gif = $(this).attr('data-name');
     displayGifs();
+});
+
+// Animate on hover ------------------------------------------------
+$(document).on('mouseover', '.gif', function() {
+    $(this).attr('src', $(this).data('animate'));
+    $(this).attr('data-state', 'animate');
+});
+$(document).on('mouseout', '.gif', function() {
+    $(this).attr('src', $(this).data('still'));
+    $(this).attr('data-state', 'still');
 });
 
 // Search for new gifs ---------------------------------------------
